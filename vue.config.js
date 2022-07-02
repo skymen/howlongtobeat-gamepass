6aec56ed60726fe886cfdfb819112b40459633c3
Ossama <skymen75019@gmail.com>
2022-07-02 06:30:00 +0200

    fixed build failing

--- vue.config.js
+++ vue.config.js
@@ -14,6 +14,33 @@ module.exports = {
             filter: ["**/*"],
           },
         ],
+        appId: "io.dedra.howlongtobeatgamepass",
+        files: ["dist/**/*.{js,css,html,ttf}"],
+        directories: {
+          buildResources: "./",
+        },
+        mac: {
+          category: "public.app-category.developer-tools",
+          artifactName: "${productName}-${version}-${os}-${arch}.${ext}",
+          hardenedRuntime: true,
+          entitlementsInherit: "build/entitlements.mac.inherit-plist",
+        },
+        linux: {
+          target: "AppImage",
+          artifactName: "${productName}-${version}-${os}-${arch}.${ext}",
+          category: "Development",
+        },
+        win: {
+          target: "nsis",
+          artifactName: "${productName}-${version}-${os}-${arch}.${ext}",
+        },
+        publish: {
+          provider: "github",
+          repo: "howlongtobeat-gamepass",
+          owner: "skymen",
+          releaseType: "release",
+        },
+        // afterSign: "./build/afterSignHook.js",
       },
       nodeIntegration: true,
     },
